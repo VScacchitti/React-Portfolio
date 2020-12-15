e15b5e8d39b7f6ac20f355eb51524596c44de789
Vince Scacchitti <VScacchitti24@gmail.com>
2020-12-15 17:25:50 -0500

    created component, and pages folder, added header, navbar and main js components. Added contact, home, portionfolio pages.

---
+++ portfolio/src/components/NavBar.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function NavBar () {
+    return (
+        <nav className="navbar fixed-top navbar-dark bg-primary text-white">
+          <a className="navbar-brand" href="https://www.instagram.com/vpscacchitti/"
+            >Vince Scacchitti</a
+          >
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <section className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <section className="navbar-nav">
+              <a className="nav-link active" href="index.html"
+                >About Me <span class="sr-only">(current)</span></a
+              >
+              <a className="nav-link" href="">Portfolio</a>
+              <a className="nav-link" href="">Contact</a>
+            </section>
+          </section>
+        </nav>
+    )
+}
+
+export default NavBar;
