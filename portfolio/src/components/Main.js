83338eabc53a208a60e671393eeb3ca51194fa3a
Vince Scacchitti <VScacchitti24@gmail.com>
2020-12-21 09:49:11 -0500

    added basic footer component, copied images into image folder.

---
+++ portfolio/src/components/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import AboutMe from "./AboutMe"
+
+
+function Main () {
+    return (
+        <div>
+            < Header/>
+            <AboutMe />
+        </div>
+    )
+}
+
+export default Main;
--- portfolio/src/components/NavBar.js
+++ portfolio/src/components/NavBar.js
@@ -4,8 +4,7 @@ function NavBar () {
     return (
         <nav className="navbar fixed-top navbar-dark bg-primary text-white">
           <a className="navbar-brand" href="https://www.instagram.com/vpscacchitti/"
-            >Vince Scacchitti</a
-          >
+            >Vince Scacchitti</a>
           <button
             className="navbar-toggler"
             type="button"
@@ -22,8 +21,8 @@ function NavBar () {
               <a className="nav-link active" href="index.html"
                 >About Me <span class="sr-only">(current)</span></a
               >
-              <a className="nav-link" href="">Portfolio</a>
-              <a className="nav-link" href="">Contact</a>
+              <a className="nav-link" >Portfolio</a>
+              <a className="nav-link" >Contact</a>
             </section>
           </section>
         </nav>
