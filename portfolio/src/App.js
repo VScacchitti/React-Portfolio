1640b979fef44ffc3c883c51f1d0822d4d8d0aff
Vince Scacchitti <VScacchitti24@gmail.com>
2020-12-22 06:42:06 -0500

    added header, footer and about me on page.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from "../src/components/Header"
-
+import Header from "../src/components/Header";
+import AboutMe from "../src/components/AboutMe"
+import Footer from "../src/components/Footer"
 function App() {
   return (
     <div className="App">
       <Header />
+      <AboutMe/>
+      <Footer/>
     </div>
   );
 }
--- portfolio/src/components/AboutMe.js
+++ portfolio/src/components/AboutMe.js
@@ -39,3 +39,5 @@ function AboutMe () {
         </div>
     )
 }
+
+export default AboutMe;
